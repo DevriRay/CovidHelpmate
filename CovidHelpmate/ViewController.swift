ae0aeb33c93ee39edd382b5f166878c32b91b638
Devri Ray <raydevri@gmail.com>
2020-08-05 10:02:20 -0500

    Initial commit

---
+++ CovidHelpmate/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CovidHelpmate
+//
+//  Created by BYNC on 8/3/20.
+//  Copyright © 2020 BYNC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func helpfulResourcesButton(_ sender: UIButton) {
+    }
+    @IBAction func testingSitesButton(_ sender: UIButton) {
+    }
+    
+
+}
+
